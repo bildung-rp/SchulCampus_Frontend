4f188333fb9d56e73324b2cbc0234ba25cc4789b
Alexandra Kaulfuß <Alexandra.Kaulfuss@pl.rlp.de>
2019-06-14 09:12:36 +0200

    Anpassungen

--- Header.php
+++ Header.php
@@ -16,7 +16,6 @@
 
     <link rel="stylesheet" href="CSS/style.css">
     <link rel="stylesheet" href="CSS/mobile.css">
-    <link rel="stylesheet" href="CSS/costomie.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
