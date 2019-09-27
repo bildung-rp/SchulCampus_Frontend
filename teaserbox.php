7fa959cc183dc2202540111d9d8257059cb7ad86
Axel Jindra <axel.jindra@pl.rlp.de>
2019-09-27 23:24:10 +0200

    insert dummy image, shadow refinement

--- teaserbox.php
+++ teaserbox.php
@@ -4,7 +4,7 @@
     <div class="symbol">
         <i class="sc-icon icon-folder-open"></i>
     </div>
-    <img class="info-teaser-image" src="images/tafel.jpg" alt="tafel">
+    <img class="info-teaser-image" src="https://picsum.photos/200/150" alt="dummy image">
     <a href="Infopage.php" class="info-teaser-link">
         <h3>Hallo ich bin der Link zur Quelle!</h3>
         <p>Bei mir gibt es Vieles zu entdecken!</p>
