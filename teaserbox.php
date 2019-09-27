a87135cfdc0b726b1d7bdc36b9529993281be7b1
Axel Jindra <axel.jindra@pl.rlp.de>
2019-09-28 00:22:34 +0200

    fixed link to infopage.php

--- teaserbox.php
+++ teaserbox.php
@@ -5,7 +5,7 @@
         <i class="sc-icon icon-folder-open"></i>
     </div>
     <img class="info-teaser-image" src="https://picsum.photos/200/150" alt="dummy image">
-    <a href="Infopage.php" class="info-teaser-link">
+    <a href="infopage.php" class="info-teaser-link">
         <h3>Hallo ich bin der Link zur Quelle!</h3>
         <p>Bei mir gibt es Vieles zu entdecken!</p>
     </a>
